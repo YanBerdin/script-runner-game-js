360f0759f6ef9994195880bd456870a2459b00c8
YanBerdin <yandevformation@gmail.com>
2024-02-26 19:09:32 +0100

    documentation & comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+/**
+ * Represents the main application object.
+ * @namespace
+ * @property {number} nbRows - The number of rows in the game board.
+ * @property {number} nbCols - The number of columns in the game board.
+ * @property {object} start - The starting position of the game.
+ * @property {number} start.row - The row of the starting position.
+ * @property {number} start.col - The column of the starting position.
+ * @property {object} end - The ending position of the game.
+ * @property {number} end.row - The row of the ending position.
+ * @property {number} end.col - The column of the ending position.
+ * @property {object} current - The current position of the game.
+ * @property {number} current.row - The row of the current position.
+ * @property {number} current.col - The column of the current position.
+ * @property {string} direction - The direction of the current position (top, right, bottom, left).
+ */
 const app = {
   nbRows: 4,
   nbCols: 6,
@@ -12,8 +28,6 @@ const app = {
   },
 
   current: null,
-
-  //TODO delay: 500,
 
   init: function () {
     console.log("init");
@@ -33,19 +47,25 @@ const app = {
       .addEventListener("click", app.handleLaunchScriptButton);
   },
 
+  // Position de la case de départ
   randomStart: function () {
     app.start.row = 1;
     app.start.col = 1;
+    // Position aléatoire
     // app.start.row = Math.floor(Math.random() * Math.floor(app.nbRows)) + 1;
     // app.start.col = Math.floor(Math.random() * Math.floor(app.nbCols)) + 1;
   },
+
+  // Position de la case de fin
   randomEnd: function () {
     app.end.row = 4;
     app.end.col = 6;
+    // Position aléatoire
     // app.end.row = Math.floor(Math.random() * Math.floor(app.nbRows)) + 1;
     // app.end.col = Math.floor(Math.random() * Math.floor(app.nbCols)) + 1;
   },
 
+  // Initialisation du jeu
   initGame: function () {
     app.current = {
       row: app.start.row,
@@ -54,37 +74,42 @@ const app = {
     app.direction = "right"; // top, right, bottom, left
   },
 
-  // Lancer l'interprétation du script au click sur le bouton "Lancer le script"
+  // Récupération et filtrage du script au click sur le bouton "Lancer le script"
   handleLaunchScriptButton: function () {
     console.log("bouton cliqué");
 
     let errorMessage = "";
 
     // Récupérer saisie utilisateur
-    //? split("\n")  divise la string en un Array de sous-chaînes,
+    // split("\n")  divise la string en un Array de sous-chaînes,
     // avec retour de ligne ("\n") comme séparateur
     // 1 ligne de commande = 1 ligne dans le tableau
     // let codeLines = document.getElementById("userCode").value.split("\n");
     let userCode = document.getElementById("userCode").value;
-    //? Filter aussi les lignes vides
+
+    // Déclarer un array de lignes de script
+    // Filter pour éliminer les lignes vides
     let codeLines = userCode.split("\n").filter((line) => line.trim() !== "");
 
-    // Vérifier si le tableau de lignes de code est vide
+    // Vérifier si un script a été saisi
     if (codeLines.length === 0) {
       console.log("Le script est vide. Empêcher la soumission.");
       // alert("Saisir un script avant de lancer !");
+
+      // Afficher message d'erreur
       errorMessage += "Saisir un script avant de lancer !";
       document.getElementById("errorMessages").textContent =
         " Le script ne peut être vide avant de lancer ! ";
       return; // Ne pas soumettre le formulaire
     }
 
-    // Initialiser le jeu et afficher le plateau
+    // Initialiser la commande et afficher le plateau (sans effacer le script)
     app.initGame();
     app.drawBoard();
     document.getElementById("errorMessages").textContent = "";
 
-    //? La fonction JS `window.setTimeout()` => Définit une fonction à exécuter après un délai en millisecondes
+    //? Lancer la boucle de lecture du tableau de script
+    // La fonction JS `window.setTimeout()` => Définit une fonction à exécuter après un délai en millisecondes
     // la fonction anonyme passée en paramètre de window.setTimeout()
     //  sera exécutée après un délai de 500 millisecondes
     window.setTimeout(function () {
@@ -93,17 +118,21 @@ const app = {
     }, 500); //TODO app.delay
   },
 
-  //? La fonction codeLineLoop() prend 2 paramètres :
+  // => Boucler sur les lignes de script à interpréter
+  // une par une (avec délai de 500ms entre chaque ligne)
+  // => Appeler checkSuccess() à la fin .
+  // codeLineLoop() prend 2 paramètres :
   //  un tableau de lignes de code et un index.
   codeLineLoop: function (codeLines, index) {
     let errorMessage = "";
 
-    //? Récupérer la ligne de code actuelle
-    //? à partir du tableau de lignes de code en utilisant l’index fourni.
+    // Récupérer la ligne de code actuelle
+    // à partir du tableau de lignes de code en utilisant l’index fourni.
     // Getting currentLine
     // let currentLine = codeLines[index];
     // console.log(currentLine);
 
+    // trim() supprime les espaces en début et fin de string
     let currentLine = codeLines[index].trim();
     console.log(currentLine);
 
@@ -111,15 +140,14 @@ const app = {
 
     if (continueReading) {
       app.drawBoard();
-      // Ensuite, elle incrémente l’index pour passer à la ligne suivante.
-      // Increment
+      // Incrément de l’index pour passer à la ligne suivante.
       index++;
 
       //? Si la fin du tableau de lignes de code n’est pas atteinte,
       if (index < codeLines.length) {
-        //? la fonction appelle app.codeLineLoop() avec un délai de 1000 ms
-        //? Permet d’interpréter chaque ligne de code une par une
-        //? avec 1 sec d'intervalle
+        // la fonction appelle app.codeLineLoop() avec un délai de 1000 ms
+        // Permet d’interpréter chaque ligne de code une par une
+        // avec 1 sec d'intervalle
         window.setTimeout(function () {
           app.codeLineLoop(codeLines, index);
         }, 500); //TODO app.delay
@@ -138,6 +166,7 @@ const app = {
     }
   },
 
+  // Interpréter une ligne de script
   interpretLine: function (line) {
     console.log("interpretLine() appelé");
     console.log(line);
@@ -159,9 +188,9 @@ const app = {
         return false;
       }
     } else {
-      // alert('MEGA ERROR ! Commande inconnue "' + line + '"');
+      // alert('SYNTAX ERROR ! Commande inconnue "' + line + '"');
       errorMessage += 'SYNTAX ERROR ! Commande inconnue "' + line + '"';
-      console.log(errorMessage);
+      // console.log(errorMessage);
       document.getElementById("errorMessages").textContent +=
         '  SYNTAX ERROR ! Commande inconnue "' + line + '"';
       return false;
@@ -170,17 +199,13 @@ const app = {
     return true;
   },
 
-  //? Cette fonction permet d’interpréter chaque ligne de code d’un tableau
-  //? une par une avec un délai d’une seconde entre chaque ligne
-  //? et d’appeler checkSuccess() à la fin .
-
-  // Display if the game is won or not
+  // Vérifier si la partie est gagnée
   checkSuccess: function () {
     console.log("checkSuccess() appelé");
     if (app.current.row == app.end.row && app.current.col == app.end.col) {
       // alert("Gagné !");
       document.getElementById("errorMessages").textContent = "  Gagné !  ";
-      console.log("Gagné !");
+      // console.log("Gagné !");
     }
   },
 
@@ -261,28 +286,29 @@ const app = {
     }
 
     // Hors limite => return false
-    // 1ere ligne
+    // 1ere ligne (bordure supérieure)
     if (app.current.row < 1) {
       // Ligne réinitialisée à 1
       app.current.row = 1;
       console.log("out 1");
       return false;
     }
-    // derniere ligne
+    // derniere ligne (bordure inférieure)
     else if (app.current.row > app.nbRows) {
       // ligne réinitialisée au nbr de lignes maximum valide
+      // positionne le curseur à la limite autorisée
       app.current.row = app.nbRows;
       console.log("out 2");
       return false;
     }
-    // 1ere colonne
+    // 1ere colonne (bordure gauche)
     else if (app.current.col < 1) {
       // Colonne réinitialisée à 1
       app.current.col = 1;
       console.log("out 3");
       return false;
     }
-    // Derniere colonne
+    // Derniere colonne (bordure droite)
     else if (app.current.col > app.nbCols) {
       // Colonne réinitialisée au nbr de colonnes maximum valide
       app.current.col = app.nbCols;
